26b366bf83205e9775edb749da14462e6af2777e
Pietro Delsante <p.delsante@certego.net>
2016-06-29 22:49:56 +0200

    Fixing wrong email

--- backend/start.sh
+++ backend/start.sh
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 #
-# Author:   Pietro Delsante <p.delsante AT gmail.com>
+# Author:   Pietro Delsante <pietro.delsante AT gmail.com>
 #
 
 # Send all output (stdout + stderr) to a log file
